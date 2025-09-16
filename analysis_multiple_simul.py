4fceffa833e36d031d83aa4545fec4df3990532b
Leonardo Razzai <leonardo.razzai2@unibo.it>
2025-09-16 12:07:36 +0200

    put each plot in its function for cleaness

--- analysis_multiple_simul.py
+++ analysis_multiple_simul.py
@@ -58,62 +58,94 @@ def plot_3d_TdMOTconc():
     plt.clf()
 
 
-for dMOT in dMOT_range:
-    for i, T in enumerate(T_range):
-        label = f'T = {T} μK, dMOT = {dMOT} mm'
-        print(label)
+def plot_cap_frac_vs_T(T_range, dMOT_range, img_folder):
+    """
+    Plot capture fraction vs. time for fixed dMOT and varying T.
+    Saves one plot per dMOT.
+    """
+    cmap = colormaps.get_cmap('inferno')
+    colors = [cmap(x) for x in np.linspace(0.1, 0.8, len(T_range))]
 
-        ts, f_cap = capt_atoms_vs_t(T, dMOT)
-        plot_cap_frac(ts, f_cap, label=label, color=colors[i])
+    for dMOT in dMOT_range:
+        for i, T in enumerate(T_range):
+            label = f'T = {T} μK, dMOT = {dMOT} mm'
+            print(label)
 
-    plt.legend()
-    plt.savefig(img_folder + f'cap_frac_dMOT={dMOT}mm.jpg')
-    plt.clf()
+            ts, f_cap = capt_atoms_vs_t(T, dMOT)
+            plot_cap_frac(ts, f_cap, label=label, color=colors[i])
 
-
-# --- 2. Density distributions at fiber for fixed dMOT, varying T ---
-for dMOT in dMOT_range:
-    for i, T in enumerate(T_range):
-        label = f'T = {T} μK, dMOT = {dMOT} mm'
-        print(label)
-
-        hist_rho_step, _ = density_at_fib(step=-1, T=T, dMOT=dMOT)
-        plot_density_at_fib(hist_rho_step=hist_rho_step, label=label, color=colors[i])
-
-    plt.legend()
-    plt.savefig(img_folder + f'density_at_fib_dMOT={dMOT}mm.jpg')
-    plt.clf()
+        plt.legend()
+        plt.savefig(img_folder + f'cap_frac_dMOT={dMOT}mm.jpg')
+        plt.clf()
 
 
-# --- 3. Capture fraction vs. time at fixed T, varying dMOT ---
-cmap = colormaps.get_cmap('YlGnBu')
-colors = [cmap(x) for x in np.linspace(0.3, 0.7, len(dMOT_range))]
+def plot_density_vs_T(T_range, dMOT_range, img_folder):
+    """
+    Plot radial density distribution at fiber for fixed dMOT and varying T.
+    Saves one plot per dMOT.
+    """
+    cmap = colormaps.get_cmap('inferno')
+    colors = [cmap(x) for x in np.linspace(0.1, 0.8, len(T_range))]
 
-for T in T_range:
-    for i, dMOT in enumerate(dMOT_range):
-        label = f'T = {T} μK, dMOT = {dMOT} mm'
-        print(label)
+    for dMOT in dMOT_range:
+        for i, T in enumerate(T_range):
+            label = f'T = {T} μK, dMOT = {dMOT} mm'
+            print(label)
 
-        ts, f_cap = capt_atoms_vs_t(T, dMOT)
-        plot_cap_frac(ts, f_cap, label=label, color=colors[i])
+            hist_rho_step, _ = density_at_fib(step=-1, T=T, dMOT=dMOT)
+            plot_density_at_fib(hist_rho_step=hist_rho_step, label=label, color=colors[i])
 
-    plt.legend()
-    plt.savefig(img_folder + f'cap_frac_T={T}uK.jpg')
-    plt.clf()
-
-
-# --- 4. Density distributions at fiber for fixed T, varying dMOT ---
-for T in T_range:
-    for i, dMOT in enumerate(dMOT_range):
-        label = f'T = {T} μK, dMOT = {dMOT} mm'
-        print(label)
-
-        hist_rho_step, _ = density_at_fib(step=-1, T=T, dMOT=dMOT)
-        plot_density_at_fib(hist_rho_step=hist_rho_step, label=label, color=colors[i])
-
-    plt.legend()
-    plt.savefig(img_folder + f'density_at_fib_T={T}uK.jpg')
-    plt.clf()
+        plt.legend()
+        plt.savefig(img_folder + f'density_at_fib_dMOT={dMOT}mm.jpg')
+        plt.clf()
 
 
-plot_3d_TdMOTconc()
+def plot_cap_frac_vs_dMOT(T_range, dMOT_range, img_folder):
+    """
+    Plot capture fraction vs. time for fixed T and varying dMOT.
+    Saves one plot per T.
+    """
+    cmap = colormaps.get_cmap('YlGnBu')
+    colors = [cmap(x) for x in np.linspace(0.3, 0.7, len(dMOT_range))]
+
+    for T in T_range:
+        for i, dMOT in enumerate(dMOT_range):
+            label = f'T = {T} μK, dMOT = {dMOT} mm'
+            print(label)
+
+            ts, f_cap = capt_atoms_vs_t(T, dMOT)
+            plot_cap_frac(ts, f_cap, label=label, color=colors[i])
+
+        plt.legend()
+        plt.savefig(img_folder + f'cap_frac_T={T}uK.jpg')
+        plt.clf()
+
+
+def plot_density_vs_dMOT(T_range, dMOT_range, img_folder):
+    """
+    Plot radial density distribution at fiber for fixed T and varying dMOT.
+    Saves one plot per T.
+    """
+    cmap = colormaps.get_cmap('YlGnBu')
+    colors = [cmap(x) for x in np.linspace(0.3, 0.7, len(dMOT_range))]
+
+    for T in T_range:
+        for i, dMOT in enumerate(dMOT_range):
+            label = f'T = {T} μK, dMOT = {dMOT} mm'
+            print(label)
+
+            hist_rho_step, _ = density_at_fib(step=-1, T=T, dMOT=dMOT)
+            plot_density_at_fib(hist_rho_step=hist_rho_step, label=label, color=colors[i])
+
+        plt.legend()
+        plt.savefig(img_folder + f'density_at_fib_T={T}uK.jpg')
+        plt.clf()
+
+
+
+if __name__ == "__main__":
+    plot_cap_frac_vs_T(T_range, dMOT_range, img_folder)
+    plot_density_vs_T(T_range, dMOT_range, img_folder)
+    plot_cap_frac_vs_dMOT(T_range, dMOT_range, img_folder)
+    plot_density_vs_dMOT(T_range, dMOT_range, img_folder)
+    plot_3d_TdMOTconc()
