0dc582f5d93f442b8ebe037e5aadb204a6f61ed4
Leonardo Razzai <leonardo.razzai2@unibo.it>
2026-01-29 14:39:18 +0100

    add heating, change rages

--- run_multiple_simul.py
+++ run_multiple_simul.py
@@ -17,11 +17,12 @@ if beam_name not in ['Gauss', 'LG']:
 beam = beams[beam_name]
 
 T_range = np.arange(start=5, stop=50, step=5) # uK
-dMOT_range = np.arange(start=42, stop=54, step=4) # mm
+dMOT_range = np.arange(start=2, stop=22, step=2) # mm
+HEATING = True
 
 for T in T_range:
     for dMOT in dMOT_range:
         label = f'T = {T} uK, dMOT = {dMOT} mm, Beam = {beam.name}'
         print(label)
-        simulation(N=1e5, T=T, dMOT=dMOT, beam=beam)
+        simulation(N=1e5, T=T, dMOT=dMOT, beam=beam, Heating=HEATING)
         time.sleep(60)
